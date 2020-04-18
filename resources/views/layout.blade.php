5d801a0765ae225b8af2414c5929a7962076139a
Mads Bakholt <mads@bakholt.dk>
2020-04-18 22:19:57 +0200

    Added conditional Active state to Bootstrap sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,10 +18,10 @@
         <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
             <ul class="nav nav-pills flex-column">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/">Customers <span class="sr-only">(current)</span></a>
+                    <a class="nav-link {!! Request::is('/') ? 'active' : '' !!}" href="/">Customers <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/invoices">Invoices</a>
+                    <a class="nav-link {!! Request::is('invoices') ? 'active' : '' !!}" href="/invoices">Invoices</a>
                 </li>
             </ul>
 
